24ef7d1bbe59f0c4449ad743363acea70da318a6
ict02-05 <ict02-05@DESKTOP-4AF3KI8>
2023-10-05 15:10:31 +0900

    good

---
+++ front-end/src/heplers/axios_helper.js
@@ -0,0 +1,34 @@
+import axios from 'axios'; // npm install axios 부터
+
+axios.defaults.baseURL = 'http://localhost:8083';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+// 로그인 완료시 JWT 저장
+// getAuthToken
+export const getAuthToken = () => {
+    return window.localStorage.getItem("auth_token");
+}
+// setAuthToken
+export const setAuthToken = (token) => {
+    window.localStorage.setItem("auth_token", token);
+}
+
+export const request = (method, url, data) => {
+
+    // let headers = {};
+    // if(getAuthToken() != null && getAuthToken != "null") {
+    //     headers = {"Authorization": 'Bearer ${getAuthToken()}'}; // 토큰 요청 헤더 => Authorization : <type> <credentials> 
+    //     console.log('headers: ', headers);
+    // }
+    console.log('axios');
+    console.log('method : ', method);
+    console.log('url : ', url);
+    console.log('data : ', data);
+
+    return axios({
+        method: method,
+        // headers: headers,
+        url: url,
+        data: data
+    });
+}
